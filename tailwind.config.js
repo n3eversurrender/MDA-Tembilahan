21f92a7fb5e973515f370d7e395c8f834f734d18
Zaini Ridha <zaiiridha8@gmail.com>
2024-06-13 16:52:10 +0700

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./src/**/*.{html,js}
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
